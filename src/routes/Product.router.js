ba11fd3e2ddfe5041d3b7e6b0b5255f13429181f
tonie <anthophoenix24@gmail.com>
2024-06-21 22:52:53 +0300

    employ a get method for fetching data

--- src/routes/Product.router.js
+++ src/routes/Product.router.js
@@ -1,12 +1,28 @@
 import { Router } from "express";
+import pool from "../db.config";
 
 const router = Router();
 //products
-router.get(" ", (req, res) => {
-  res.send("get all product");
+router.get(" ", async (req, res) => {
+  try {
+    const feedback = await pool.query("SELECT * FROM products");
+    res.status(200).json({ sucess: true, data: feedback.rows });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 });
-router.get(" id:", (req, res) => {
-  res.send("get a product");
+router.get(" id:", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const feedback = await pool.query("SELECT * FROM products WHERE id=$1"[id]);
+    if (feedback.rowCount == 0) {
+      res.status(404).json({ success: false, message: "product not found" });
+    } else {
+      res.status(200).json({ sucess: true, data: feedback.rows[0] });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 });
 router.post("/ id:", (req, res) => {
   res.send("add a product");
